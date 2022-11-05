8f52fe8257cc67018a172c557dc1deec8e5f1bb3
bagasysf <bagasysf@gmail.com>
2022-11-05 09:19:27 +0700

    merperbaik icon rocket

--- src/components/HeroIlustration.jsx
+++ src/components/HeroIlustration.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const HeroIllustration = () => {
   return (
     <>
-      <div className="absolute top-0 right-0 z-10 h-[70px] w-[70px]">
+      <div className="absolute top-0 right-0 -z-10 h-[70px] w-[70px]">
         <img
-          className="mt-[60px] -ml-[50%] scale-[5.0] md:-ml-[60%] lg:ml-[20%]"
+          className="mt-[60px] -ml-[70%] scale-[5.0] md:-ml-[70%] lg:ml-[20%]"
           src={process.env.PUBLIC_URL + "/images/hero-img.png"}
           alt="header-illustration"
         ></img>
