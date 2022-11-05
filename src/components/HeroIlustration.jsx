109c1fca01078df06606438bb0d698fa0fcb5467
bagasysf <bagasysf@gmail.com>
2022-11-05 09:12:04 +0700

    masih soal rocket icon

--- src/components/HeroIlustration.jsx
+++ src/components/HeroIlustration.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const HeroIllustration = () => {
   return (
     <>
-      <div className="absolute right-0 -z-10 h-[100px] w-[100px]">
+      <div className="absolute top-0 right-0 z-10 h-[70px] w-[70px]">
         <img
-          className="absolute mt-[60px] -ml-[40px] scale-[4.0] md:-ml-[50px] lg:ml-[20px]"
+          className="mt-[60px] -ml-[50%] scale-[5.0] md:-ml-[60%] lg:ml-[20%]"
           src={process.env.PUBLIC_URL + "/images/hero-img.png"}
           alt="header-illustration"
         ></img>
